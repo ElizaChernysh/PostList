5865c0090e58b9613816ad8b0ba043d2c128cb07
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2022-08-18 11:45:36 +0300

    change

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/PostList/'
-    : '/'
+    : '/',
+  
+    pwa: {
+      workboxOptions: {
+        exclude: [/_redirects/]
+      }
+    }
 }
